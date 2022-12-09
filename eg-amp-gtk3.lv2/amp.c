1c8c96261e63bb56d9cd40d3d91fc67b07735ef3
lherg <lherg@home.fr>
2022-12-09 18:58:07 +0100

    gtk3 ok

--- eg-amp-gtk3.lv2/amp.c
+++ eg-amp-gtk3.lv2/amp.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 
-#define AMP_URI "http://lv2plug.in/plugins/eg-amp-gtk2"
+#define AMP_URI "http://lv2plug.in/plugins/eg-amp-gtk3"
 
 typedef enum { AMP_GAIN = 0, AMP_INPUT = 1, AMP_OUTPUT = 2 } PortIndex;
 
