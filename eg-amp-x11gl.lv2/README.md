c884abd4c294da36e6871d751ae4a9fe860e9381
lherg <lherg@home.fr>
2022-12-20 13:03:44 +0100

    Add x11gl

--- eg-amp-x11gl.lv2/README.md
+++ eg-amp-x11gl.lv2/README.md
@@ -1,3 +1,15 @@
+Still in work...<br/>
+First compile pugl<br/>
+```
+git clone https://gitlab.com/lv2/pugl.git
+cd pugl && meson setup build
+cd build && meson compile
+```
+
+Then compile the application:<br/>
+Have a look:<br/>
+https://stackoverflow.com/questions/49138195/whats-the-difference-between-rpath-link-and-l
+
 Compile<g/>
 ```
 make
--- eg-amp-x11gl.lv2/amp_ui.c
+++ eg-amp-x11gl.lv2/amp_ui.c
@@ -3,71 +3,29 @@
 // Copyright 2011-2016 David Robillard <d@drobilla.net>
 // SPDX-License-Identifier: ISC
 
+#include "pugl/cairo.h"
+#include "pugl/pugl.h"
+
+#include <cairo.h>
+
 #include "lv2/core/lv2.h"
 #include "lv2/core/lv2_util.h"
 #include "lv2/ui/ui.h"
 
-#include <gtk/gtk.h>
-//#include <iostream>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define AMP_UI_URI "http://lv2plug.in/plugins/eg-amp-x11gl#ui"
 
 typedef struct {
   LV2UI_Write_Function write;
   LV2UI_Controller     controller;
-
-  // Optional show/hide interface
-  GtkWidget* window;
-  GtkWidget* box;
-  GtkWidget* hscale;   
-  GtkAdjustment* adjustment;
-  GtkWidget* label;
-
+  PuglNativeView parentWindow;
   bool       did_init;
 } AmpUI;
-
-/** Send current UI settings to backend. */
-static void
-send_ui_state(LV2UI_Handle handle)
-{
-  AmpUI*  ui   = (AmpUI*)handle;
-  const float gain = (float)gtk_range_get_value(GTK_RANGE(ui->hscale ));
-  //std::cout << "UI/send_ui_state/gain:" << gain << "\n";
-  ui->write (ui->controller, 0, sizeof (float), 0, &gain);
-
-}
-
-/** Gtk widget callback. */
-static gboolean
-on_cfg_changed(GtkWidget* widget, gpointer data)
-{
-  AmpUI* ui = (AmpUI*)data;
-  send_ui_state(data);
-
-  return TRUE;
-}
-
-static void
-destroy_window(AmpUI* ui)
-{
-  if (ui->window) {
-    gtk_container_remove(GTK_CONTAINER(ui->window), ui->box);
-    gtk_widget_destroy(ui->window);
-    ui->window = NULL;
-  }
-}
-
-static gboolean
-on_window_closed(GtkWidget* widget, GdkEvent* event, gpointer data)
-{
-  AmpUI* ui = (AmpUI*)data;
-
-  // Remove widget so Gtk doesn't delete it when the window is closed
-  gtk_container_remove(GTK_CONTAINER(ui->window), ui->box);
-  ui->window = NULL;
-
-  return FALSE;
-}
 
 static LV2UI_Handle
 instantiate(const LV2UI_Descriptor*   descriptor,
@@ -78,36 +36,7 @@ instantiate(const LV2UI_Descriptor*   descriptor,
             LV2UI_Widget*             widget,
             const LV2_Feature* const* features)
 {
-  AmpUI* ui = (AmpUI*)calloc(1, sizeof(AmpUI));
-  if (!ui) {
-    return NULL;
-  }
-
-  ui->write      = write_function;
-  ui->controller = controller;
-  *widget        = NULL;
-  ui->window     = NULL;
-  ui->did_init   = false;
-
-  // Construct Gtk UI
-
-  ui->box = gtk_hbox_new(FALSE, 4);
-  
-  ui->adjustment = gtk_adjustment_new (0.0, -90.0, 24.0, 0.1, 5.0, 0.0);
-  ui->hscale      = gtk_hscale_new((GtkAdjustment *)ui->adjustment );
-  ui->label = gtk_label_new("Gain (dB): ");
-  gtk_widget_set_size_request(ui->hscale, 620, 60);
-
-  gtk_container_set_border_width(GTK_CONTAINER(ui->box), 4);
-  
-  gtk_box_pack_start(GTK_BOX(ui->box), ui->label, TRUE, TRUE, 0); 
-  gtk_box_pack_start(GTK_BOX(ui->box), ui->hscale, TRUE, TRUE, 0);
-  
-  g_signal_connect(
-    G_OBJECT(ui->hscale), "value-changed", G_CALLBACK(on_cfg_changed), ui);
-
-  *widget = ui->box;
-
+	AmpUI* ui = (AmpUI*)calloc(1, sizeof(AmpUI));
   return ui;
 }
 
@@ -116,13 +45,6 @@ cleanup(LV2UI_Handle handle)
 {
   AmpUI* ui = (AmpUI*)handle;
 
-  if (ui->window) {
-    destroy_window(ui);
-  }
-  g_object_unref(ui->adjustment);
-  gtk_widget_destroy(ui->hscale);
-  gtk_widget_destroy(ui->box);
-  free(ui);
 }
 
 /* receive information from DSP */
@@ -136,7 +58,6 @@ port_event(LV2UI_Handle handle,
   AmpUI* ui = (AmpUI*)handle;
 	if (port_index == 0) {
     const float _gain       = *(float*)buffer;
-    gtk_range_set_value (GTK_RANGE(ui->hscale ),_gain);
 
 	}  
 }
@@ -146,31 +67,6 @@ static int
 ui_show(LV2UI_Handle handle)
 {
   AmpUI* ui = (AmpUI*)handle;
-
-  if (ui->window) {
-    return 0;
-  }
-
-  if (!ui->did_init) {
-    int argc = 0;
-    gtk_init_check(&argc, NULL);
-    g_object_ref(ui->box);
-    ui->did_init = true;
-  }
-  ui->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  //gtk_window_set_title(GTK_WINDOW(ui->window), "Glh-AmP");
-  //gtk_window_set_default_size(GTK_WINDOW(ui->window ), 600, 200);
-  //gtk_container_set_border_width(GTK_CONTAINER(ui->window ), 4); 
-
-  gtk_container_add(GTK_CONTAINER(ui->window), ui->box);
-
-  g_signal_connect(
-    G_OBJECT(ui->window), "delete-event", G_CALLBACK(on_window_closed), handle);
-
-  gtk_widget_show_all(ui->window);
-
-  gtk_window_present(GTK_WINDOW(ui->window));
-
   return 0;
 }
 
@@ -179,11 +75,6 @@ static int
 ui_hide(LV2UI_Handle handle)
 {
   AmpUI* ui = (AmpUI*)handle;
-
-  if (ui->window) {
-    destroy_window(ui);
-  }
-
   return 0;
 }
 
@@ -192,9 +83,6 @@ static int
 ui_idle(LV2UI_Handle handle)
 {
   AmpUI* ui = (AmpUI*)handle;
-  if (ui->window) {
-    gtk_main_iteration_do(false);
-  }
   return 0;
 }
 
